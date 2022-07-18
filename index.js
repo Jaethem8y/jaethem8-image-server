d56ae82c2dfcb571315de91810cdc95b64ce6978
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-07-18 17:34:08 +0900

    change inbound

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 app.use(express.static("public"));
-app.listen(8000, () => {
+app.listen(8080, () => {
   console.log("server is running");
 });
