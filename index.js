bde4c52aac7ac46d4282b6eedca219289115d6c1
jaehyeok choi <jaethem8@jaehyeoks-MacBook-Pro.local>
2022-07-18 20:48:10 +0900

    add java projects

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const app = express();
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Jaethem8 image server");
+});
+
 app.listen(8080, () => {
   console.log("server is running");
 });
